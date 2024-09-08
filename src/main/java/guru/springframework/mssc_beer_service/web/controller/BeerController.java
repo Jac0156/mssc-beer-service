4e70843c7e770843afc74fdb7de0978f0799b294
Jack Weby <jac0156@hotmail.com>
2024-09-08 11:26:15 -0400

    Ajouter Error Handler to BeerController

--- src/main/java/guru/springframework/mssc_beer_service/web/controller/BeerController.java
+++ src/main/java/guru/springframework/mssc_beer_service/web/controller/BeerController.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,13 +28,13 @@ public class BeerController {
     }
     
     @PostMapping
-    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity<BeerDto> saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
         // TODO implementation
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity<BeerDto> updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity<BeerDto> updateBeerById(@PathVariable("beerId") UUID beerId, @Validated @RequestBody BeerDto beerDto) {
         // TODO implementation
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
---
+++ src/main/java/guru/springframework/mssc_beer_service/web/controller/MvcExceptionHandler.java
@@ -0,0 +1,24 @@
+package guru.springframework.mssc_beer_service.web.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import jakarta.validation.ConstraintViolationException;
+
+@ControllerAdvice
+public class MvcExceptionHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> validationErrorHandler(ConstraintViolationException ex){
+        List<String> errorsList = new ArrayList<>(ex.getConstraintViolations().size());
+
+        ex.getConstraintViolations().forEach(error -> errorsList.add(error.toString()));
+
+        return new ResponseEntity<>(errorsList, HttpStatus.BAD_REQUEST);
+    }
+}
