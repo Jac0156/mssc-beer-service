32221ae457a026b2bb01c0dd5371b1355db04662
Jack Weby <jac0156@hotmail.com>
2024-09-30 18:04:54 -0400

    Corriger bootstrap beerloader. Ajouter Hikary param.

--- src/test/java/guru/springframework/mssc_beer_service/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
+++ src/test/java/guru/springframework/mssc_beer_service/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import guru.springframework.mssc_beer_service.bootstrap.BeerLoader;
-
 @Disabled
 @SpringBootTest
 class BeerInventoryServiceRestTemplateImplTest {
@@ -22,9 +20,9 @@ class BeerInventoryServiceRestTemplateImplTest {
 
     @Test
     void getOnHandInventory() {
-        Integer qoh = beerInventoryService.getOnHandInventory(BeerLoader.BEER_1_UUID);
+        //Integer qoh = beerInventoryService.getOnHandInventory(BeerLoader.BEER_1_UUID);
 
-        System.out.println("Inventaire actuel: " + qoh);
+        //System.out.println("Inventaire actuel: " + qoh);
 
     }
 }
